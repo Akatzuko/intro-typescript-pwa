b09dc134e6be8fb737d4b6278ddb21cdaa8a807d
JuanCarlos <carliitosakatsukiito@gmail.com>
2022-09-10 12:05:43 -0500

    nueva actualizacion

---
+++ src/temas-ts/01-tipos-datos.ts
@@ -0,0 +1,20 @@
+
+
+console.log('Hola Mundo!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+var t1:number | string =3;
+let t2:number | string =2;
+let activo:boolean=false;
+
+const t3 = 666;
+
+t1='zzz';
+t2='zzz';
+
+console.log(t1)
+
+
