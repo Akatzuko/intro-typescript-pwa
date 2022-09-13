8c7e3cbbc5573a13d673cc4fba2f9a64d05cbc1a
JuanCarlos <carliitosakatsukiito@gmail.com>
2022-09-12 20:36:34 -0500

    obj-array-inter

---
+++ src/temas-ts/02-obj-array-inter.ts
@@ -0,0 +1,37 @@
+
+let nombres:string[]=['mario','dario','juan']
+
+console.log(nombres);
+
+let datos:(boolean|number|string)[]=['mario','23',true,'Lopez']
+
+console.log(datos);
+
+datos.push(200);
+datos.push('Mariano');
+datos.push(false);
+
+console.log(datos);
+
+//-----------------------------------------
+//otro tipo de crear arreglos
+let arreglo: Array<number>=[1,2,3,4]
+
+interface Alumno{
+    matricula:String,
+    nombre:string,
+    edad:number,
+    email:string,
+    materias?:String[]
+}
+
+const alumno:Alumno={
+
+    matricula :'19002283',
+    nombre :'Juan Carlos',
+    edad: 23,
+    email:'gmail.com',
+    materias: ['Ingles']
+}
+
+console.log(alumno);
